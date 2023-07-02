c44201431944d8db9b7c1d04c2d46bbf8b1ba478
mironovisa <mironovisa@gmail.com>
2023-07-02 23:42:54 +0300

    Reinstall Node

--- App.tsx
+++ App.tsx
@@ -3,10 +3,14 @@ import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import RootNavigator from "./src/navigators/RootNavigator";
 import { Amplify } from "aws-amplify";
-import { withAuthenticator } from "@aws-amplify/ui-react-native";
+import {
+  useAuthenticator,
+  withAuthenticator,
+} from "@aws-amplify/ui-react-native";
 import awsExports from "./src/aws-exports";
 import { TryContextProvider } from "./src/context/tryoutCont";
-
+import "react-native-url-polyfill/auto";
+import "react-native-get-random-values";
 
 Amplify.configure(awsExports);
 function App() {
